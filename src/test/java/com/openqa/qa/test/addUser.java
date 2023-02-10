6a311a3312eba189539d444973a74380c60e2a76
HemantDhavle <96099050+HemantDhavle@users.noreply.github.com>
2023-02-10 15:35:25 +0530

    Create addUser.java

---
+++ src/test/java/com/openqa/qa/test/addUser.java
@@ -0,0 +1,20 @@
+package com.openqa.qa.test;
+
+public class addUser 
+{
+	public void logDetails()
+	{
+		String fname ="hemant";
+		String lname="dhavle";
+		int age=23;
+    String city = "Indore";
+    String country = "India";
+		System.out.println("user first name is "+fname);
+		System.out.println("user last name is "+lname);
+		System.out.println("user age is "+age);
+  System.out.println("user city name is "+city);
+    System.out.println("user country name is "+country);
+		
+	}
+
+}
