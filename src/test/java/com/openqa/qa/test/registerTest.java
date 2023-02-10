425bd01835d2c808e5dec83d1c754a56e2465a5b
hemantd17@gmail.com <hemantd17@gmail.com>
2023-02-10 12:49:48 +0530

    add new method into register class

--- src/test/java/com/openqa/qa/test/registerTest.java
+++ src/test/java/com/openqa/qa/test/registerTest.java
@@ -3,5 +3,10 @@ package com.openqa.qa.test;
 public class registerTest 
 {
 	String name ="hemant";
+	
+	public void doRegistration()
+	{
+		System.out.println(name +"  registered successfully");
+	}
 
 }
